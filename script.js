681bdbaa33b4dd0a638752e919578599446b63ea
HeitorDalla <heitorvillavilla@gmail.com>
2025-06-17 14:41:55 -0300

    Começado projeto de javascript

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 "use strict";
 
+document.addEventListener("DOMContentLoaded", () => {
+    const botoesEnviar = document.querySelectorAll(".button");
+    
+    botoesEnviar.forEach((button) => {
+        button.addEventListener("click", (e) => {
+            e.preventDefault();
+
+            const plano = e.currentTarget.parentElement.classList[1];
+            console.log(plano)
+
+            window.location.href = `assinatura.html?plano=${plano}`;
+        })
+    })
+})
